af90ca8bc52f723da4d3a2990e71cff3ec779ca5
Pedro Felipe Arruda <pedrofelipega@gmail.com>
2022-06-28 14:59:17 -0400

    Transforma imagem em cartoon

---
+++ Projetos/Cartoon_Image/main.py
@@ -0,0 +1,26 @@
+#importação das bibliotecas
+import cv2
+
+imagem = "Homem1.jpg"
+
+#ler imagem
+img = cv2.imread(imagem)
+
+# Limites - Edges
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray = cv2.medianBlur(gray, 5)
+edges = cv2.adaptiveThreshold(gray, 255,
+        cv2.ADAPTIVE_THRESH_MEAN_C,
+        cv2.THRESH_BINARY, 9, 9)
+
+# Cartoonização
+color = cv2.bilateralFilter(img, 9, 250, 250)
+cartoon = cv2.bitwise_and(color, color, mask = edges)
+
+cv2.imshow("Image", img)
+cv2.imshow("edges", edges)
+cv2.imshow("Cartoon", cartoon)
+cv2.imwrite(imagem.split()[0]+"_Cartoon.png",cartoon)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
